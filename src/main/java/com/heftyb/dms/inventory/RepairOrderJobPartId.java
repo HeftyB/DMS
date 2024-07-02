07d5cc2958f8b6f9d00567043c01790f975c8020
Andrew Shields <35949416+HeftyB@users.noreply.github.com>
2024-07-02 02:08:39 -0400

    RepairOrderJobPart, Stocked Part, Part changes

---
+++ src/main/java/com/heftyb/dms/inventory/RepairOrderJobPartId.java
@@ -0,0 +1,44 @@
+package com.heftyb.dms.inventory;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class RepairOrderJobPartId implements Serializable {
+    private long repairOrderJob;
+    private long part;
+
+    public RepairOrderJobPartId() {
+    }
+
+    public long getRepairOrderJob() {
+        return repairOrderJob;
+    }
+
+    public void setRepairOrderJob(long repairOrderJob) {
+        this.repairOrderJob = repairOrderJob;
+    }
+
+    public long getPart() {
+        return part;
+    }
+
+    public void setPart(long part) {
+        this.part = part;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RepairOrderJobPartId)) return false;
+        RepairOrderJobPartId that = (RepairOrderJobPartId) o;
+        return getRepairOrderJob() == that.getRepairOrderJob() && getPart() == that.getPart();
+    }
+
+    @Override
+    public int hashCode() {
+        return 61;
+    }
+}
