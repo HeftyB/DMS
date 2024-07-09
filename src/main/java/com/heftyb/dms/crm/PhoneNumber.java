ace532f48219fca4b1696734b350d3077909ca46
HeftyB <ashields0923@gmail.com>
2024-07-09 12:33:54 -0400

    phoneNumber

---
+++ src/main/java/com/heftyb/dms/crm/PhoneNumber.java
@@ -0,0 +1,69 @@
+package com.heftyb.dms.crm;
+
+import com.heftyb.dms.account.models.Auditable;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "phoneNumbers")
+public class PhoneNumber extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String number;
+
+    @ManyToOne
+    @JoinColumn
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn
+    private SaleLead lead;
+
+    public PhoneNumber() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public SaleLead getLead() {
+        return lead;
+    }
+
+    public void setLead(SaleLead lead) {
+        this.lead = lead;
+    }
+}
---
+++ src/main/java/com/heftyb/dms/crm/repositories/PhoneNumberRepository.java
@@ -0,0 +1,4 @@
+package com.heftyb.dms.crm.repositories;
+
+public interface PhoneNumberRepository {
+}
