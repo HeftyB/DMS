68094fa5e54f957c95a8b00cd4a826bf8362bc47
HeftyB <ashields0923@gmail.com>
2024-07-09 12:10:33 -0400

    repository load 2

---
+++ src/main/java/com/heftyb/dms/repairorder/repositories/RepairOrderJobRepository.java
@@ -0,0 +1,7 @@
+package com.heftyb.dms.repairorder.repositories;
+
+import com.heftyb.dms.repairorder.RepairOrderJob;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepairOrderJobRepository extends CrudRepository<RepairOrderJob, Long> {
+}
---
+++ src/main/java/com/heftyb/dms/repairorder/repositories/RepairOrderRepository.java
@@ -0,0 +1,7 @@
+package com.heftyb.dms.repairorder.repositories;
+
+import com.heftyb.dms.repairorder.RepairOrder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepairOrderRepository extends CrudRepository<RepairOrder, Long> {
+}
