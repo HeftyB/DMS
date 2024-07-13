d116b031e7885bdb01f7e9195333cdee01185be6
HeftyB <ashields0923@gmail.com>
2024-07-13 17:21:21 -0400

    Invoice + Items, Statement + Items, Status

---
+++ src/main/java/com/heftyb/dms/account/models/Status.java
@@ -0,0 +1,5 @@
+package com.heftyb.dms.account.models;
+
+public enum Status {
+    OPEN, CLOSED
+}
---
+++ src/main/java/com/heftyb/dms/account/models/invoice/InvoiceType.java
@@ -0,0 +1,5 @@
+package com.heftyb.dms.account.models.invoice;
+
+public enum InvoiceType {
+    REPAIR_ORDER, COUNTER_TICKET, VEHICLE_SALE
+}
