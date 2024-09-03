f80cc5c9a117712a74efd62c8d16a57980a3293c
HeftyB <ashields0923@gmail.com>
2024-09-02 23:26:08 -0400

    added repository methods

--- src/main/java/com/heftyb/dms/timekeeping/repositories/TimeClockPunchSetRepository.java
+++ src/main/java/com/heftyb/dms/timekeeping/repositories/TimeClockPunchSetRepository.java
@@ -1,7 +1,14 @@
 package com.heftyb.dms.timekeeping.repositories;
 
+import com.heftyb.dms.account.PayPeriod;
+import com.heftyb.dms.crm.Employee;
 import com.heftyb.dms.timekeeping.TimeClockPunchSet;
+import com.heftyb.dms.users.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TimeClockPunchSetRepository extends CrudRepository<TimeClockPunchSet, Long> {
+    List<TimeClockPunchSet> findByEmployee(Employee employee);
+    List<TimeClockPunchSet> findByEmployeeAndPayPeriod(Employee employee, PayPeriod period);
 }
