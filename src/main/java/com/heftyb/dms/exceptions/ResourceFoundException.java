0cc9c8bbdbb8959c9e2ce565e3b70f860b66538b
HeftyB <ashields0923@gmail.com>
2024-09-02 23:24:21 -0400

    update message

--- src/main/java/com/heftyb/dms/exceptions/ResourceFoundException.java
+++ src/main/java/com/heftyb/dms/exceptions/ResourceFoundException.java
@@ -2,6 +2,6 @@ package com.heftyb.dms.exceptions;
 
 public class ResourceFoundException extends RuntimeException {
     public ResourceFoundException(String message) {
-        super("Error from a BloomTech Application " + message);
+        super(message);
     }
 }
