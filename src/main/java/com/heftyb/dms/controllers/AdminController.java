4602c69fef56edcb618568e37ea99c9952414033
HeftyB <ashields0923@gmail.com>
2024-10-23 14:01:41 -0400

    AdminController - added routes for creating an Employee

--- src/main/java/com/heftyb/dms/controllers/AdminController.java
+++ src/main/java/com/heftyb/dms/controllers/AdminController.java
@@ -1,11 +1,15 @@
 package com.heftyb.dms.controllers;
 
+import com.heftyb.dms.crm.Employee;
+import com.heftyb.dms.crm.services.EmployeeService;
 import com.heftyb.dms.users.User;
 import com.heftyb.dms.users.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,17 +20,13 @@ import java.util.List;
 public class AdminController {
 
     private final UserService userService;
+    private final EmployeeService employeeService;
 
-    public AdminController(final UserService userService) {
+    public AdminController(final UserService userService,
+                           final EmployeeService employeeService) {
         this.userService = userService;
+        this.employeeService = employeeService;
     }
-
-//    @GetMapping("/")
-//    public ModelAndView adminHome(HttpServletRequest request, ModelMap model, Principal principal) {
-//        User u = userService.findUserByUsername(principal.getName());
-//        model.addAttribute("user", u);
-//        return new ModelAndView("administrator", model);
-//    }
 
     @GetMapping({"/", ""})
     public String adminHome(HttpServletRequest request, Model model, Principal principal) {
@@ -36,6 +36,22 @@ public class AdminController {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
         model.addAttribute("username", principal.getName());
-        return "administrator";
+        return "hr_administrator";
+    }
+
+    @GetMapping({"/create_employee", "/create_employee/"})
+    public String create_employee_form(Principal principal, ModelMap map) {
+        map.addAttribute("username", principal.getName());
+
+        return "employee_form";
+    }
+
+    @PostMapping({"/create_employee", "/create_employee/"})
+    public String create_employee(Employee employee) {
+
+
+        employeeService.save(employee);
+
+        return "redirect:/admin/";
     }
 }
