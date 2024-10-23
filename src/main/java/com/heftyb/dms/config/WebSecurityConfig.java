2133ee87f816624bf218cb28b2888372291e4f9b
HeftyB <ashields0923@gmail.com>
2024-10-23 15:40:35 -0400

    OpenController

--- src/main/java/com/heftyb/dms/config/WebSecurityConfig.java
+++ src/main/java/com/heftyb/dms/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +26,8 @@ public class WebSecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
                         .requestMatchers("/admin/**")
                         .hasRole("ADMIN")
+                        .requestMatchers("/", "/style.css", "/oil.jpg")
+                        .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
