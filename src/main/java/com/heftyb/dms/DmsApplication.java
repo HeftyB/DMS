e01501b18ef7e52fd1235a08aaf1fa7ef7c4783d
HeftyB <ashields0923@gmail.com>
2024-12-14 14:46:16 -0500

    moved auditing annotation

--- src/main/java/com/heftyb/dms/DmsApplication.java
+++ src/main/java/com/heftyb/dms/DmsApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties(StorageConfigurationProperties.class)
 @EnableScheduling
+@EnableJpaAuditing
 public class DmsApplication {
 
     public static void main(String[] args) {
